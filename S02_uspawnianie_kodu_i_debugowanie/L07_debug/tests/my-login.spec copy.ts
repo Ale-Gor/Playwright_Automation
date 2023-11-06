8a63f6c596e576b1f327a9aef75d932dd70dcc85
Oleksii Kosinski <79alegor@gmail.com>
2023-11-06 18:39:25 +0100

    my-files are added

---
+++ S02_uspawnianie_kodu_i_debugowanie/L07_debug/tests/my-login.spec copy.ts
@@ -0,0 +1,59 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('User login to Demobank', () => {  
+  test.beforeEach(async ({ page }) => {
+    // const url = 'https://demo-bank.vercel.app/';
+    // await page.goto(url);
+
+    await page.goto('/')
+
+  });
+
+
+  test.only('successful login with correct credentials', async ({ page }) => {
+    // Arrange
+    const userId = 'testerLO';
+    const userPassword = '10987654';
+    const expectedUserName = 'Jan Demobankowy';
+
+    //Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+
+    //Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
+  });
+
+  test('unsuccessful login with too short username', async ({ page }) => {
+    // Arrange
+    const userId = 'tester';
+    const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
+
+    //Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').click();
+
+    //Assert
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedErrorMessage,
+    );
+  });
+
+  test('unsuccessful login with too short password', async ({ page }) => {
+    // Arrange
+    const userId = 'testerLO';
+    const password = '1234';
+    const errorPasswordMessage = 'hasło ma min. 8 znaków';
+
+    //Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('password-input').blur();
+
+    //Assert
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      errorPasswordMessage,
+    );
+  });
+});
