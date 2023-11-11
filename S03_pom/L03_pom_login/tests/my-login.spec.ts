898adb285b4b1745de13c3f15810f0b344e3f7b6
Oleksii Kosinski <79alegor@gmail.com>
2023-11-11 13:43:15 +0100

    page added

---
+++ S03_pom/L03_pom_login/tests/my-login.spec.ts
@@ -0,0 +1,62 @@
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
+import { LoginPage } from '../pages/my-login.page';
+
+test.describe('User login to Demobank', () => {
+  test.beforeEach(async ({ page }) => {
+    // const url = 'https://demo-bank.vercel.app/';
+    // await page.goto(url);
+
+    await page.goto('/')
+
+  });
+
+
+  test('successful login with correct credentials', async ({ page }) => {
+    // Arrange
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
+    const expectedUserName = 'Jan Demobankowy';
+
+    //Act
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userId);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
+
+    //Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
+  });
+
+  test('unsuccessful login with too short username', async ({ page }) => {
+    // Arrange
+    const userId = 'tester';
+    const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
+
+    //Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').click();
+
+    //Assert
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedErrorMessage,
+    );
+  });
+
+  test('unsuccessful login with too short password', async ({ page }) => {
+    // Arrange
+    const userId = loginData.userId;
+    const password = '1234';
+    const errorPasswordMessage = 'hasło ma min. 8 znaków';
+
+    //Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(password);
+    await page.getByTestId('password-input').blur();
+
+    //Assert
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      errorPasswordMessage,
+    );
+  });
+});
